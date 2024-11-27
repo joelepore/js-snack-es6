d9c263bbac19e5c7ec6fb21cb88eea4be1d41d40
joelepore <giuseppejoelepore@gmail.com>
2024-11-27 16:27:58 +0100

    feat: create array of objects (bikes)

---
+++ jsnack1/script.js
@@ -0,0 +1,18 @@
+/*
+## Snack 1
+Creare un array di oggetti:
+
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+Stampare a schermo la bici con peso minore.
+*/
+const biciclette = [
+  { nome: "Trek", peso: 8.5 },
+  { nome: "Cannondale", peso: 9.2 },
+  { nome: "Specialized", peso: 8.8 },
+  { nome: "Bianchi", peso: 9.0 },
+  { nome: "Scott", peso: 8.7 },
+  { nome: "Giant", peso: 9.1 },
+  { nome: "Pinarello", peso: 8.4 },
+  { nome: "Cervélo", peso: 8.9 }
+];
