946fbc94d94fd27beb61b8cc385a012f6412aa8b
joelepore <giuseppejoelepore@gmail.com>
2024-11-28 10:01:00 +0100

    fix: push squadra.nome instead of squadra.puntiFatti

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,6 +7,8 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 const squadreCalcio = [
   { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
@@ -30,7 +32,7 @@ const squadreInfo = [];
 // Output
 for (let squadra of squadreCalcio) {
   squadreInfo.push({
-    puntiFatti: squadra.puntiFatti,
+    nome: squadra.nome,
     falliSubiti: squadra.falliSubiti
   })
 }
